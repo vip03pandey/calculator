c099087741bed8510a349502e28fb7a544d3fc38
vip03pandey <vipul03pandey@gmail.com>
2023-09-20 21:36:39 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const input=document.querySelector(".input-box");
+const calc=document.querySelectorAll(".calc");
+let string = "";
+let arr = Array.from(calc);
+arr.forEach(calc => {
+    calc.addEventListener('click', (e) =>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'AC'){
+            string = "";
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'DE'){
+            string = string.substring(0, string.length-1);
+            input.value = string;
+        }
+        else{
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+        
+    })
+})
